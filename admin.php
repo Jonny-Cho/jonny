47fd22658fbfb800f720f4f50a99e601098de61c
Jonny <andante2183@gmail.com>
2017-03-06 02:37:25 +0900

    admin.php (관리자 글쓰기 화면) 추가

---
+++ admin.php
@@ -0,0 +1,99 @@
+<!-- 데이터베이스 접속 -->
+<?php
+require_once('conn.php');
+$sql  = 'SELECT * FROM `step1`';
+$result = mysqli_query($conn, $sql);
+?>
+
+<html lang="ko">
+	<head>
+		<title>Jonny</title>
+		<meta charset="utf-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1" />
+		<!--[if lte IE 8]><script src="assets/js/ie/html5shiv.js"></script><![endif]-->
+		<link rel="stylesheet" href="assets/css/main.css" />
+		<!--[if lte IE 9]><link rel="stylesheet" href="assets/css/ie9.css" /><![endif]-->
+		<!--[if lte IE 8]><link rel="stylesheet" href="assets/css/ie8.css" /><![endif]-->
+		<link rel="stylesheet" href="assets/css/style.css">
+	</head>
+  <body>
+		<div id="page-wrapper">
+      <!-- Header -->
+				<header id="header">
+					<h1 id="logo"><a href="index.php">Jonny's IT Blog</a></h1>
+					<nav id="nav">
+						<ul>
+							<li>
+								<a href="it.php">생활코딩 따라하기</a>
+								<ul>
+									<li>
+                    <a href="it.php">1단계 일주일안에 심플하게</a>
+										<!-- todo ul대신 데이터베이스 넣어야함 -->
+										<ul>
+										<?php
+											while($row = mysqli_fetch_assoc($result)){
+												echo '<li><a href="it.php?id='.$row['id'].'">'.htmlspecialchars($row['title']).'</a></li>';
+											}
+										?>
+										</ul>
+                  </li>
+									<li><a href="right-sidebar.html">2단계 홈페이지를 움직이게</a></li>
+									<li><a href="no-sidebar.html">3단계 서버/데이터베이스/보안</a></li>
+								</ul>
+							</li>
+						</ul>
+					</nav>
+				</header>
+
+      <!-- Form -->
+        <section>
+          <h3>글쓰기</h3>
+          <form method="post" action="process.php">
+            <div class="row uniform 50%">
+              <div class="12u$">
+                <input type="text" name="title" id="title" value="" placeholder="title" />
+              </div>
+              <div class="12u$">
+                <textarea name="description" id="description" placeholder="Enter your description" rows="20"></textarea>
+              </div>
+              <div class="12u$">
+                <ul class="actions">
+                  <li><input type="submit" value="Send Message" class="special" /></li>
+                  <li><input type="reset" value="Reset" /></li>
+                </ul>
+              </div>
+            </div>
+          </form>
+        </section>
+
+				</div>
+
+			<!-- Footer -->
+				<footer id="footer">
+					<ul class="icons">
+						<li><a href="#" class="icon alt fa-twitter"><span class="label">Twitter</span></a></li>
+						<li><a href="#" class="icon alt fa-facebook"><span class="label">Facebook</span></a></li>
+						<li><a href="#" class="icon alt fa-linkedin"><span class="label">LinkedIn</span></a></li>
+						<li><a href="#" class="icon alt fa-instagram"><span class="label">Instagram</span></a></li>
+						<li><a href="#" class="icon alt fa-github"><span class="label">GitHub</span></a></li>
+						<li><a href="#" class="icon alt fa-envelope"><span class="label">Email</span></a></li>
+					</ul>
+					<ul class="copyright">
+						<li>&copy; Untitled. All rights reserved.</li><li>Design: <a href="http://html5up.net">HTML5 UP</a></li>
+					</ul>
+				</footer>
+
+		</div>
+
+		<!-- Scripts -->
+			<script src="assets/js/jquery.min.js"></script>
+			<script src="assets/js/jquery.scrolly.min.js"></script>
+			<script src="assets/js/jquery.dropotron.min.js"></script>
+			<script src="assets/js/jquery.scrollex.min.js"></script>
+			<script src="assets/js/skel.min.js"></script>
+			<script src="assets/js/util.js"></script>
+			<!--[if lte IE 8]><script src="assets/js/ie/respond.min.js"></script><![endif]-->
+			<script src="assets/js/main.js"></script>
+
+	</body>
+</html>
